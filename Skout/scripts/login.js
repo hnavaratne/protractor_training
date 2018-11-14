c4f9aed0cf8e1cebefac66f97e930b133a1ea614
sgit2018 <shavik3harin@yahoo.com>
2018-11-13 22:44:25 -0500

    On branch SkoutFBLogin  Changes to be committed: 	modified:   Skout/commonScripts/common.js 	modified:   Skout/pages/homePage.js 	new file:   Skout/pages/whoAreYouIntPage.js 	modified:   Skout/scripts/login.js :Wq

--- Skout/scripts/login.js
+++ Skout/scripts/login.js
@@ -1,49 +1,79 @@
-"use strict";
+/* eslint-disable no-undef */
+'use strict'
 
-var login = require('../pages/homePage');
-var common = require('../commonScripts/common');
+var login = require('../pages/homePage')
+var interested = require('../pages/whoAreYouIntPage')
+var common = require('../commonScripts/common')
 
-//URL of the home page
-var homeURL = 'https://www.skout.com/';
+// URL of the home page
+var homeURL = 'https://www.skout.com/'
 
-//Data used for testing
-var userName = 'virtusa.protractor@gmail.com';
-var password = '1qaz2wsx@';
+// Data used for testing
+var userName = 'virtusa.protractor@gmail.com'
+var password = '1qaz2wsx@'
+var goMeetBtn = element(by.xpath('//*[text()="Go meet someone"]'))
+var skoutLogo = element(by.xpath('//div[@data-emoji="Protractor Testing"]'))
+var fbAgreeBtn = element(by.xpath('//*[text()="Agree"]'))
+var fbEmail = element(by.id('email'))
+var fbPassword = element(by.id('pass'))
 
+// Test login with correct credentials
 describe('Login to the page', function () {
-    xit('Login to the page with valid credentials', function () {
-        common.firstThings();
-        common.navigateToPage(homeURL);
-        browser.sleep(5000);
-        login.loginHomePage(userName, password);
-        browser.sleep(5000);
+  xit('Login to the page with valid credentials', function () {
+    common.firstThings()
+    common.navigateToPage(homeURL)
+    browser.sleep(5000)
+    login.loginHomePage(userName, password)
+    browser.sleep(5000)
+    expect(goMeetBtn.isPresent()).toBe(true)
+    interested.interestWomen()
+    browser.sleep(5000)
+    expect(skoutLogo.isPresent()).toBe(true)
+  })
 
-    });
+  // Test adding a new user
+  xit('Test adding New User', function () {
+    var gender = 'Man'
+    var interest = 'Women'
+    common.firstThings()
+    common.navigateToPage(homeURL)
+    browser.sleep(5000)
+    login.newUser('James Plunket', 'June', '15', '1990', gender, interest)
+    browser.sleep(5000)
+  })
+  // Test clicking on Google play
+  xit('Test Link for Google play', function () {
+    login.googlePlayClick()
+    common.switchTab()
+    var EC = protractor.ExpectedConditions
+    EC.urlContains('https://play.google.com/store/apps/details?id=com.skout.android')
+  })
 
-    it('New User', function () {
-        var gender = 'Man';
-        var interest ='Women';
-        common.firstThings();
-        common.navigateToPage(homeURL);
-        browser.sleep(5000);
-        login.newUser('James Plunket', 'June', '15', '1990', gender, interest);
-        browser.sleep(5000);
-    });
-
-    xit('Google play', function () {
-        login.googlePlayClick();
-        common.switchTab();
-        var EC = protractor.ExpectedConditions;
-        browser.wait(EC.urlContains('https://play.google.com/store/apps/details?id=com.skout.android'), 5000);
-    });
-
-
-    xit('App Store', function () {
-        login.appStoreClick();
-        common.switchTab();
-        var EC = protractor.ExpectedConditions;
-        browser.wait(EC.urlContains('https://itunes.apple.com/us/app/skout/id302324249'), 5000);
-    });
-
-
-});
+  // Test going to Apple Store
+  xit('Test Link for App Store', function () {
+    login.appStoreClick()
+    common.switchTab()
+    var EC = protractor.ExpectedConditions
+    EC.urlContains('https://itunes.apple.com/us/app/skout/id302324249')
+  })
+  // Login with incorrect credentials
+  xit('Test incorrect credentials', function () {
+    common.firstThings()
+    common.navigateToPage(homeURL)
+    browser.sleep(5000)
+    login.loginHomePage('virtusa.protractor@yahoo.com', password)
+    browser.sleep(5000)
+    expect(goMeetBtn.isPresent()).toBe(true)
+  })
+  // Test the FB login
+  it('Test login with Facebook', function () {
+    common.firstThings()
+    common.navigateToPage(homeURL)
+    login.clickOnFB()
+    expect(fbAgreeBtn.isPresent()).toBe(true)
+    login.acceptFBTerm()
+    expect(fbEmail.isPresent()).toBe(true)
+    expect(fbPassword.isPresent()).toBe(true)
+    login.submitFBCreds(userName, password)
+  })
+})
